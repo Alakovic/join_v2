e60dd63abff19a1787c34ea8f9ce7f9695c35b30
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-02-11 13:58:20 +0100

    limit setted , message when limit achieved

--- script/stakeholder.js
+++ script/stakeholder.js
@@ -1,3 +1,30 @@
 document.getElementById("logo").addEventListener("click", () => {
   window.location.href = "../index.html";
 });
+
+async function init() {
+  updateCount();
+  setInterval(updateCount, 2000);
+}
+
+async function updateCount() {
+  let count = await getEmailCount();
+  document.getElementById("requestCount").textContent = count;
+  let text = document.getElementById("request");
+  let limitMsg = document.querySelector(".limit_message");
+
+  if (count >= 10) {
+    text.style.color = "red";
+    limitMsg.style.display = "flex";
+  } else {
+    text.style.color = "";
+    limitMsg.style.display = "none";
+  }
+}
+
+async function getEmailCount() {
+  let response = await fetch(
+    "https://join-5677e-default-rtdb.europe-west1.firebasedatabase.app/emailCounter/count.json",
+  );
+  return (await response.json()) ?? 0;
+}
